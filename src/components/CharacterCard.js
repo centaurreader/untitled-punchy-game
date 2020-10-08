480124cc544903c72c9f7bd7d7a60f20c04bb384
Paul Browning <centaur@centaurreader.com>
2020-10-07 23:00:46 -0500

    get card values from props

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,25 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CharacterCard = () => {
+const CharacterCard = ({
+  name,
+  health,
+  moves,
+}) => {
   return (
     <section className="card">
-      <h2>Character Name</h2>
+      <h2>{name}</h2>
       <section>
         <label>
           Health
-          <input type="number" disabled value={3} />
+          <input type="number" disabled value={health} />
         </label>
       </section>
       <section>
         <h3>Moves</h3>
         <ul>
-          <li>Punch P P D</li>
-          <li>Kick K K D</li>
-          <li>Special P P K D D</li>
+          {moves.map((move) => (
+            <li key={move.name}>
+              {move.name} {move.resources.map(r => (`${r} `))}
+            </li>
+          ))}
         </ul>
       </section>
     </section>
   );
+};
+CharacterCard.propTypes = {
+  name: PropTypes.string,
+  health: PropTypes.string,
+  moves: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    resources: PropTypes.arrayOf(PropTypes.oneOf(['P', 'K', 'D'])),
+  })),
+};
+CharacterCard.defaultProps = {
+  name: 'Mutant Man',
+  health: '21',
+  moves: [
+    {
+      name: 'Punch',
+      resources: [ 'P', 'P', 'D', ],
+    },
+    {
+      name: 'Kick',
+      resources: [ 'K', 'K', 'D', ],
+    },
+    {
+      name: 'Mutator Slam',
+      resources: [ 'K', 'K', 'P', 'D', 'D', ],
+    },
+  ],
 };
 
 export default CharacterCard;
