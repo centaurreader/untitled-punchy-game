f3fee31d2833fc2786b1f04fd00a15b25fe8ff6f
Paul Browning <centaur@centaurreader.com>
2020-10-17 13:51:38 -0500

    better description

--- upg-tools/util.js
+++ upg-tools/util.js
@@ -2,7 +2,7 @@ const yargs = require('yargs');
 const Resource = require('./Resource');
 
 yargs
-  .command('build <files...>', 'Build Resources', (yarg) => {
+  .command('build <files...>', 'Build JSON Resource Files from CSVs', (yarg) => {
     return yarg.positional('files', {
       describe: 'Build CSVs into Sandbox Resourecs',
     });
